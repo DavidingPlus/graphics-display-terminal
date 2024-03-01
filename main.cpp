ae657781c7532e9f8909aa0470045f8514f0e0c5
lzx0626 <2065666169@qq.com>
2024-03-01 21:55:37 +0800

    fix basic framework

--- main.cpp
+++ main.cpp
@@ -1,11 +1,21 @@
 #include "widget.h"
 
+#include <synchapi.h>
 #include <QApplication>
+#include <QtNetwork>
+#include <QDebug>
 
-int main(int argc, char *argv[])
+#define maxBufferSize 1024
+
+int main(int argc, char* argv[])
 {
-    QApplication a(argc, argv);
-    Widget w;
-    w.show();
-    return a.exec();
+//    QApplication a(argc, argv);
+//    Widget w;
+//    w.show();
+//    return a.exec();
+
+    QTcpSocket clientSock;
+    clientSock.connectToHost(QString("106.55.60.140"), uint16_t(8080));
+
+    Sleep(1000);
 }
